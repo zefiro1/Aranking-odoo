a688256ae54f235de153e86294d2c33cdff32a2a
zefiro1 <96437008+zefiro1@users.noreply.github.com>
2022-01-29 12:44:09 +0100

    Add files via upload
    Campos añadidos Rank, Tier, champions, Partidas, win , winrate

--- aranking/models/models.py
+++ aranking/models/models.py
@@ -6,11 +6,17 @@ from odoo import models, fields, api
 class aranking(models.Model):
     _name = 'aranking.aranking'
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
+    rank = fields.Integer()
+    role = fields.Selection([('a', 'TOP'), ('b','MID'), ('c','BOT')])
+    champion = fields.Char()
+    tier = fields.Selection([('a', 'S+'), ('b','S'), ('c','A+'),('d', 'A'), ('e','B+'), ('f','B'),('g', 'C+'), ('h','C'), ('i','D+'),('j', 'D')])
+    partidas = fields.Integer()
+    win = fields.Integer()
+    winrate = fields.Char(compute="_value_pc", store=True)
     description = fields.Text()
 
-    @api.depends('value')
+    @api.depends('win','partidas')
     def _value_pc(self):
-        self.value2 = float(self.value) / 100
+
+        self.winrate = str((float(self.win) / float(self.partidas)) * 100)+"%"
+
