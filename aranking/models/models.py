a96382ba803afbc3d7a4677d98701fc98fd01005
zefiro1 <96437008+zefiro1@users.noreply.github.com>
2022-01-31 08:30:11 +0100

    Arreglado el error de float division by zero

--- aranking/models/models.py
+++ aranking/models/models.py
@@ -3,20 +3,29 @@
 from odoo import models, fields, api
 
 
+class aranking1(models.Model):
+    _name = 'aranking.aranking1'
+
+    nombre = fields.Char('Nombre')
+    rank_id = fields.Many2one('aranking.aranking', 'champion')
+
+
 class aranking(models.Model):
     _name = 'aranking.aranking'
 
     rank = fields.Integer()
-    role = fields.Selection([('a', 'TOP'), ('b','MID'), ('c','BOT')])
+    role = fields.Selection([('a', 'TOP'), ('b', 'MID'), ('c', 'BOT')])
     champion = fields.Char()
-    tier = fields.Selection([('a', 'S+'), ('b','S'), ('c','A+'),('d', 'A'), ('e','B+'), ('f','B'),('g', 'C+'), ('h','C'), ('i','D+'),('j', 'D')])
+    tier = fields.Selection(
+        [('a', 'S+'), ('b', 'S'), ('c', 'A+'), ('d', 'A'), ('e', 'B+'), ('f', 'B'), ('g', 'C+'), ('h', 'C'),
+         ('i', 'D+'), ('j', 'D')])
     partidas = fields.Integer()
     win = fields.Integer()
-    winrate = fields.Char(compute="_value_pc", store=True)
+    winrate = fields.Char(compute="_value_pc", store=True, digits=(12, 6))
     description = fields.Text()
+    player_id = fields.One2many('aranking.aranking1', 'rank_id', 'nombre')
 
-    @api.depends('win','partidas')
+    @api.depends('win', 'partidas')
     def _value_pc(self):
-
-        self.winrate = str((float(self.win) / float(self.partidas)) * 100)+"%"
-
+        if self.partidas > 0:
+            self.winrate = str(((float(self.win) / float(self.partidas)) * 100)) + "%"
